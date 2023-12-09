d779255ff8144ff867480e1c34522e31839bc716
Bright <bright.sablah@outlook.com>
2023-12-09 01:32:31 +0000

    debug task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,94 +1,61 @@
 #include "lists.h"
 
 /**
- * _strlen_recursion - returns length of a string
- * @s: string to be assessed
- * Return: length of string
+ * reverse_list - reverse a linked list
+ * @head: first item in a linked list
+ * Return: reversed list
  */
-
-int _strlen_recursion(char *s)
+listint_t *reverse_list(listint_t *head)
 {
-	if (*s == '\0')
-		return (0);
-	return (_strlen_recursion(s + 1) + 1);
+	listint_t *prev = NULL;
+	listint_t *current = head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	return (prev);
 }
 
 /**
- * check_palin - check if string is a palindrome
- * @s: string to be assessed
- * @start: first index
- * @end: end index
- * Return: 1 if palindrome, 0 if not palindrome
- */
-
-int check_palin(char *s, int start, int end)
-{
-	if (s[start] != s[end])
-		return (0);
-	if (start >= end)
-		return (1);
-	return (check_palin(s, start + 1, end - 1));
-}
-
-/**
- * is_palin - determine if string is a palindrome
- * @s: string to be assessed
- * Return: 1 if string is palindrome and 0 if not
- */
-
-int is_palin(char *s)
-{
-	int end;
-
-	if (*s == '\0')
-		return (1);
-	end = _strlen_recursion(s) - 1;
-	return (check_palin(s, 0, end));
-}
-
-/**
- * is_palindrome - check if a singly linked list is a palindrome
- *
- * @head: start of linked list
+ * is_palindrome - check if list reads the same backwards and forward
+ * @head: pointer to first item in a linked list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = NULL;
-	listint_t *p = NULL;
-	char *s;
-	int numNodes = 0;
-	int i = 0;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *forward = *head;
+	listint_t *back = NULL;
 
-	if (*head == NULL || head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	p = *head;
-	while (p != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
-		p = p->next;
-		numNodes++;
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 
-	s = malloc(sizeof(int) * numNodes);
-	if (s == NULL)
-		return (-1);
+	if (fast != NULL)
+		slow = slow->next;
 
-	temp = *head;
+	back = reverse_list(slow);
 
-	while (temp != NULL)
+	while (back != NULL)
 	{
-		s[i] = temp->n;
-		temp = temp->next;
-		i++;
+		if (forward->n != back->n)
+			return (0);
+
+		forward = forward->next;
+		back = back->next;
 	}
 
-	if (is_palin(s) == 1)
-	{
-		free(s);
-		return (1);
-	}
-	free(s);
-	return (0);
+	return (1);
 }
