fc57a7b5ae7609af0e4dc2818bb0c097493f5953
Bright <bright.sablah@outlook.com>
2024-03-11 22:27:50 +0000

    check cycle in a linked list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks cycle in singly linked list
+* @head: head of list
+*
+* Return: 1 when there is a cycle and 0 when there is no cycle
+*/
+
+int check_cycle(listint_t *head)
+{
+	listint_t *single_parse;
+	listint_t *double_parse;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	single_parse = head;
+	double_parse = head->next;
+
+	while (single_parse != double_parse)
+	{
+		if (double_parse == NULL || double_parse->next == NULL)
+			return (0);
+
+	single_parse = single_parse->next;
+	double_parse = double_parse->next->next;
+	}
+
+return (1);
+}
