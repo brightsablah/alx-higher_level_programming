c49f52d18315330ad0b151eacef981128d493333
bRight <141350864+brightsablah@users.noreply.github.com>
2024-03-25 10:05:26 +0000

    Update 9-max_integer.py - direct assignment

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == []:
@@ -6,6 +7,16 @@ def max_integer(my_list=[]):
         if i == 0:
             largestNum = my_list[i]
             i = i + 1
+        if i < len(my_list) and my_list[i] > largestNum:
+            largestNum = my_list[i]
+    return largestNum
+'''
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    largestNum = my_list[0]
+    for i in range(len(my_list)):
         if i < len(my_list) and my_list[i] > largestNum:
             largestNum = my_list[i]
     return largestNum
