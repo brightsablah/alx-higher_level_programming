e9b67081f4cc2487dc2387758e159bd1034bd3f6
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-11 21:02:11 +0000

    Update 3-print_reversed_list_integer.py
    pop list_len

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     list_len = len(my_list) - 1
     while list_len >= 0:
-        print("{}".format(my_list.pop))
+        print("{}".format(my_list.pop(list_len)))
         list_len -= 1
