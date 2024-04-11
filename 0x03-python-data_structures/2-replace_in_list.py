0d163e0c8f47dec5092595c29c05bb05727d170e
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-11 20:46:51 +0000

    Create 2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    max_idx = len(my_list) - 1
+    if idx < 0:
+        return my_list
+    elif idx > max_idx:
+        return my_list
+    else:
+        mylist[idx] = element
+        return my_list
