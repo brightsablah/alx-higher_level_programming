393dc09cce6262c384fc4e9f1dd8c340e1ebf286
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-12 04:28:28 +0000

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = []
     max_idx = len(my_list) - 1
     if idx < 0:
         return my_list
     elif idx > max_idx:
         return my_list
     else:
-        my_list.insert(idx, element)
-        return my_list
+        new_list = my_list.insert(idx, element)
+        return new_list
