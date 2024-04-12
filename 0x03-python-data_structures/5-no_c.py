dacacb83de1e2c30fc7ae7d364bc8c19777b13e5
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-12 04:44:04 +0000

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     if my_string is not None:
         for char in my_string:
             if char == 'c' or char == 'C':
-                my_string.remove(char)
+                char = ""
         return my_string
