45cc5c7208390c964d585e10f90e7711eaca114e
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 03:06:48 +0000

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     max_value = my_list[0]
     for num in my_list:
-        if num > max_num:
-            max_num = num
+        if num > max_value:
+            max_value = num
     return max_value
