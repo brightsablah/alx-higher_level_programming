7aac95ee2d81523809596c1c049461fdefeff6a9
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 01:07:26 +0000

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     new_string = ""
     copy_string = list(my_string)
     if my_string is not None:
-        for char in copy_string:
+        for char in my_string:
             if char == 'c':
                 copy_string.remove('c')
             if char == 'C':
