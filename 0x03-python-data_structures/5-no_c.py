3ece3e0a0a13d9b92fff2d0416798fc7acbd8509
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 00:52:28 +0000

    Update 5-no_c.py create new string to handle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     if my_string is not None:
         for char in my_string:
-            if char == 'c' or char == 'C':
-                char = ""
-        return my_string
+            if char =! 'c' or char =! 'C':
+                new_string += char
+        return new_string
