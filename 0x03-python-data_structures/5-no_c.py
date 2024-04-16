ccf631b32583adfca5b21f01a3f4863c45ed0c6f
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 01:05:48 +0000

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = []
+    new_string = ""
+    copy_string = list(my_string)
     if my_string is not None:
-        for char in list(my_string):
-            if char =! 'c' and char =! 'C':
-                new_string.append(char)
-        return new_string
+        for char in copy_string:
+            if char == 'c':
+                copy_string.remove('c')
+            if char == 'C':
+                copy_string.remove('C')
+        return new_string.join(copy_string)
