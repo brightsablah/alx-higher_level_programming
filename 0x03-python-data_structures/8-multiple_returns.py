bbf1cbdd28c2e140be0170e92bb0f92c09489b03
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 02:56:38 +0000

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,10 +2,7 @@
 def multiple_returns(sentence):
     return_tup = ()
     if sentence is "":
-        first_char = None
-        return_tuple = (len(list(sentence)), first_char)
-        return return_tuple
+        return_tuple = (len(list(sentence)), None)
     else:
-        first_char = sentence[0]
-        return_tuple = (len(list(sentence)), first_char)
-        return return_tuple
+        return_tuple = (len(list(sentence)), list(sentence)[0])
+    return return_tuple
