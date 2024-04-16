3686fb460ac1164b73b13c05b85e922f6e2afd2f
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-16 00:56:28 +0000

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
+    new_string = []
     if my_string is not None:
-        for char in my_string:
+        for char in list(my_string):
             if char =! 'c' and char =! 'C':
-                new_string += char
+                new_string.append(char)
         return new_string
