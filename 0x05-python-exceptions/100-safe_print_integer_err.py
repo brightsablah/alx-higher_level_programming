7edeb29fcddd8a5e2a6723734a79e9603ac845a7
bRight <141350864+brightsablah@users.noreply.github.com>
2024-04-19 04:39:12 +0000

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
-    except TypeError as e:
+    except ValueError as e:
         sys.stderr.write("Exception: " + str(e))
         return False
