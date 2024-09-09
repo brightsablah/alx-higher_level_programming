f1a66d062f4a03aeefa3b88ce5c68ea3efe2e531
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-09 23:53:12 +0100

    framework setup

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,3 +1,39 @@
+#include <Python.h>
 #include <stdio.h>
 
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
 
+/**
+ * pirnt_python_list - print info about python lists
+ * @p: object
+ * Return: 0 or error
+ */
+void print_python_list(PyObject *p)
+{
+
+
+}
+
+/**
+ * pirnt_python_bytes - print info about python bytes
+ * @p: object
+ * Return: 0 or error
+ */
+void print_python_bytes(PyObject *p)
+{
+
+
+}
+
+/**
+ * pirnt_python_float - print info about python float objects
+ * @p: object
+ * Return: 0 or error
+ */
+void print_python_float(PyObject *p)
+{
+
+
+}
