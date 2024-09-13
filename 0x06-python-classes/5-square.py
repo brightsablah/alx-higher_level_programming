8b24219f9f2ace4fffc6cb8f6121dddecacf2d81
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-13 23:51:12 +0100

    print squares of #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""New Module contains Square class"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        area = self.size*self.size
+        return area
+
+    def my_print(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
+            
