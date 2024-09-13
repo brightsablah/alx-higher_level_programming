c3769918866022c02131d95d8040b4d01d8180ff
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-13 23:28:00 +0100

    call to setter

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """New Module contains Square class"""
 
-
 class Square:
     """Square Class"""
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
