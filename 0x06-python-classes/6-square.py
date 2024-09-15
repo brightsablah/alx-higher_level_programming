61c3fedd886d8ed307bd2a60dcc10bec567c9d98
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-15 19:17:40 +0100

    -- for size removed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,8 +4,8 @@
 
 class Square:
     """Square Class"""
-    def __init__(self, size=0, position=(0,0)):
-        self.__size = size
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
         self.position = position
 
     @property
