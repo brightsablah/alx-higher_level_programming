5054d3ab7e89a3afc7aa7c616da5f730f8f9015a
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-15 18:50:31 +0100

    position tuple check

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,7 +26,7 @@ class Square:
     
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) and len(value) == 2:
+        if not isinstance(value, tuple) or len(value) == 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if  not all(isinstance(i, int) and i > 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
