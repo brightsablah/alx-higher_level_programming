5035cb31c7c02948ab66291c4d915599dd3588e2
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-20 07:32:34 +0100

    previous change does not work

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -47,8 +47,7 @@ class SinglyLinkedList:
         else:
             # Traverse the list to find the correct spot for the new node
             current = self.__head
-            while current.next_node is not None:
-                while current.next_node.data < value:
+            while current.next_node is not None and current.next_node.data < value:
                     current = current.next_node
             # Insert the new node in the correct spot
             new_node.next_node = current.next_node
