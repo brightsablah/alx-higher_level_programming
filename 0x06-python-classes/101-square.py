83d39c6b4b760c9755d1fd1c9ed501afc9da8189
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-21 20:59:50 +0100

    integer

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -27,9 +27,9 @@ class Square:
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integer")
         if not all(isinstance(i, int) and i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integer")
         self.__position = value
 
     def area(self):
