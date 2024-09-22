2a263addc60dabb9a8c445cbc562fc4aaa2810c6
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-22 22:37:28 +0100

    pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """ addition function """
     if a is not int or a is not float:
         raise TypeError("a must be an integer")
     if b is not int or b is not float:
