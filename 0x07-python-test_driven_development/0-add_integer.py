dab4ec3fa687058ac7c1d1d1e60ecba67ceea987
bright@LATITUDE <bright.sablah@outlook.com>
2024-09-23 22:57:56 +0100

    isinstance

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,11 +4,13 @@
 
 def add_integer(a, b=98):
     """ addition function """
-    if a is not int or a is not float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if b is not int or b is not float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     num1 = int(a)
     num2 = int(b)
     sum = num1 + num2
     return sum
+
+print(add_integer(3))
